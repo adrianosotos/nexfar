4ecd8ca4f26f6d41ab3d1c172337376be25823ef
Adriano <adriano.sotos@linx3.com>
2021-07-18 11:29:44 -0300

    feat: header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,20 @@
-import { HeaderContainer } from './style'
+import { HeaderContainer, DataContainer } from './style'
 import logo from '../../assets/Nexfar.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 function Header () {
     return (
       <HeaderContainer>
-        {/* <Link to="/"> */}
-          <img src={logo} />
-        {/* </Link> */}
+        <img src={logo} />
+        <DataContainer>
+          <p>NEXFAR</p>
+          <p>
+            <span><FontAwesomeIcon icon={faShoppingCart} /></span>
+            R$ 450,00
+          </p>
+          <p>Pedido mínimo R$ 150,00</p>
+        </DataContainer>
       </HeaderContainer>
     )
 }
--- client/src/components/Header/style.js
+++ client/src/components/Header/style.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.div`
+    display: flex;
     height: 90px;
+    justify-content: space-between;
     margin-bottom: 30px;
     border-bottom: 2px solid #3cba92;
     background-color: #ffffff;
@@ -13,4 +15,20 @@ export const HeaderContainer = styled.div`
       top: 25px;
       left: 70px;
     }
+`
+
+export const DataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 20%;
+  text-align: center;
+
+  span {
+    margin-right: 6px;
+  }
+
+  p {
+    margin: 0;
+  }
 `
